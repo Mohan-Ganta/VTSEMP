3bfe4345bdb08fc807d36e344a04b9ddafd15dce
CharanKondaveeti <91211319+CharanKondaveeti@users.noreply.github.com>
2024-05-30 12:09:33 +0530

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-import Dashboard from './components/Dashboard';
-import Attendance from './employee/attendance/Attendancee';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+
+// employee
+import Attendance from "./employee/attendance/Attendance";
+import Homepage from "./employee/homepage/Homepagee";
+import Leaves from "./employee/leaves/Leaves";
+import Task from "./employee/task/Task";
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/att" element={<Attendance />} />
-        <Route path="/dashboard" element={token ? <Dashboard /> : <Navigate to="/login" />} />
+
+        <Route
+          path="employee"
+          element={token ? <Dashboard /> : <Navigate to="/login" />}
+        >
+          <Route index element={<Navigate replace to="homepage" />} />
+          <Route path="homepage" element={<Homepage />} />
+          <Route path="leaves" element={<Attendance />} />
+          <Route path="task" element={<Task />} />
+          <Route path="leave" element={<Leaves />} />
+        </Route>
       </Routes>
-      
     </Router>
   );
 }
